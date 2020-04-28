6c4389845e6ae54dedc8ca642398e0d7a3eb6373
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-28 15:40:32 +0300

    updated style

--- src/main/java/vlad/petrovskyi/internetshop/controllers/shoppingcart/ShoppingCartController.java
+++ src/main/java/vlad/petrovskyi/internetshop/controllers/shoppingcart/ShoppingCartController.java
@@ -1,7 +1,6 @@
 package vlad.petrovskyi.internetshop.controllers.shoppingcart;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
