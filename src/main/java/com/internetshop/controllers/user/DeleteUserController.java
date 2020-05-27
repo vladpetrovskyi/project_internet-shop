2fecd7b8b358c6794223ff2b70eaa8799d8dc572
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-05-27 19:45:28 +0300

    deleted NumberFormatException throwing

--- src/main/java/com/internetshop/controllers/user/DeleteUserController.java
+++ src/main/java/com/internetshop/controllers/user/DeleteUserController.java
@@ -14,7 +14,7 @@ public class DeleteUserController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException, NumberFormatException {
+            throws IOException {
         String userId = req.getParameter("user_id");
         userService.delete(Long.valueOf(userId));
 
